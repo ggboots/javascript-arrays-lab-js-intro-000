f7aaf3b1393dc5cabf92682fcfbef3936c21bda3
George Boots <ggboots.99@gmail.com>
2020-06-22 08:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  kittens.push()
+  kittens.push(name)
 }
 
 function destructivelyPrependKitten(name) {
-  kittens.unshift()
+  kittens.unshift(name)
 }
 
 function destructivelyRemoveLastKitten(name) {
